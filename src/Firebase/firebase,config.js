fe085b02ad348fad53f335aeef9d391902389fbb
Coder-the-noob <moinmatubbar6@gmail.com>
2025-11-20 19:09:43 +0600

    env file change

--- src/Firebase/firebase,config.js
+++ src/Firebase/firebase,config.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCYTQzkfVmjnFaRRUjlQ0Jdl1sCesBpSuU",
-  authDomain: "dragon-news-breaking-c6c54.firebaseapp.com",
-  projectId: "dragon-news-breaking-c6c54",
-  storageBucket: "dragon-news-breaking-c6c54.firebasestorage.app",
-  messagingSenderId: "453047698122",
-  appId: "1:453047698122:web:ff06389ae0dae5bf792a33"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
